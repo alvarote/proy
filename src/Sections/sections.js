4e0642905668368aa5aeb33ae7704150de56e5fe
alv <alvja69@gmail.com>
2021-04-13 21:29:41 -0500

    error corregido

--- src/Sections/sections.js
+++ src/Sections/sections.js
@@ -4,7 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button'
 import { Toolbar, Typography } from '@material-ui/core';
 import {BrowserRouter as Router,Route,Link} from 'react-router-dom'; 
-import Jovenes from '../Sections/jovenes'; 
+
 
 
 
@@ -17,12 +17,12 @@ export default function SectionH({url}) {
          <Router> 
            
             <Button variant="outlined" spacing={2} > 
-            <Link to={"/jovenes"}></Link>Jovenes
+            <Link to={"/Components/jovenes"}></Link>Jovenes
              </Button>
           
         
           <Button variant="outlined" > 
-          <Link to={"/cultura"}></Link>Cultura  
+          <Link to={"/Components/cultura"}></Link>Cultura  
           </Button>
        
         
